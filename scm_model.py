110443d2861555a4e47d526561848f7cc9310af1
Hossein Mohebbi <mhmohebbi@gmail.com>
2024-11-10 00:02:13 -0500

    scm updated exp

--- scm_model.py
+++ scm_model.py
@@ -11,20 +11,20 @@ def generate_data(N: int) -> pd.DataFrame:
     action_space = np.round(np.arange(0.05, 1.05, 0.05), 2)
 
     # Weights
-    w_A = 0.3
+    w_A = 0.75
 
     w_C1 = {
-        'TimeOfDay': {'Morning': 0.1, 'Afternoon': 0.0, 'Evening': 0.2, 'Night': -0.1},
+        'TimeOfDay': {'Morning': -0.1, 'Afternoon': 0.0, 'Evening': 0.2, 'Night': -0.1},
         'DayOfWeek': {'Monday': 0.0, 'Tuesday': 0.0, 'Wednesday': 0.0, 'Thursday': 0.0,
                     'Friday': 0.0, 'Saturday': 0.1, 'Sunday': 0.1},
         'Seasonality': {'Spring': 0.0, 'Summer': 0.1, 'Fall': -0.1, 'Winter': -0.2}
     }
 
     w_C2 = {
-        'Age': {'18-24': 0.1, '25-34': 0.05, '35-44': 0.0, '45-54': -0.05, '55+': -0.1},
+        'Age': {'18-24': 0.1, '25-34': 0.05, '35-44': 0.0, '45-54': -0.05, '55+': -0.2},
         'Gender': {'Male': 0.05, 'Female': -0.05},
-        'PurchaseHistory': {'No purchases': -0.2, '1-5 purchases': -0.1, '6-10 purchases': 0.1, '10+ purchases': 0.2},
-        'DeviceType': {'Mobile': 0.1, 'Desktop': 0.0, 'Tablet': -0.1}
+        'PurchaseHistory': {'No purchases': -0.3, '1-5 purchases': -0.1, '6-10 purchases': 0.1, '10+ purchases': 0.3},
+        'DeviceType': {'Mobile': 0.3, 'Desktop': 0.0, 'Tablet': -0.2}
     }
 
     # Probabilities for C2 given U 
